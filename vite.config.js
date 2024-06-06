b81de1ad0e908a378c60c17759c9bc6bf583b8eb
Hardin <hardisonpaulino@gmail.com>
2024-06-06 10:39:25 -0400

    config: basic project structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import postcss from "vite-plugin-postcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
