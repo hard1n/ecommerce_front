441eb4ba51d53ff49de7c0b56362741c20653bf2
Hardin <hardisonpaulino@gmail.com>
2024-06-16 10:55:01 -0400

    Fix: Image Sliderr added slide on swipe wit hreact-swipeable

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useSwipeable } from "react-swipeable";
+import { PropTypes } from "prop-types";
 import { FaChevronRight } from "react-icons/fa6";
 import { FaChevronLeft } from "react-icons/fa6";
 
 const Carousel = ({ children: slides }) => {
   const [imgIndex, setImgIndex] = useState(0);
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => nextSlide(),
+    onSwipedRight: () => prevSlide(),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true,
+  });
 
   const prevSlide = () => {
     setImgIndex(imgIndex === 0 ? slides.length - 1 : imgIndex - 1);
@@ -12,49 +21,55 @@ const Carousel = ({ children: slides }) => {
   const nextSlide = () => {
     setImgIndex(imgIndex === slides.length - 1 ? 0 : imgIndex + 1);
   };
+
+  const goToSlide = (index) => {
+    setImgIndex(index);
+  };
+
+  // useEffect(), [];
   return (
     <>
-      <section className="flex relative ">
+      <section className="relative w-full overflow-hidden no-scrollbar">
         <figure
-          className="flex relative transition-transform ease-in-out overflow-x-scroll no-scrollbar"
+          {...handlers}
+          className="flex bg-grayish-blue transition-transform ease-in-out duration-300"
           style={{
-            overflowBehaviorX: "contain",
-            scrollSnapType: "x mandatory",
+            // overflowBehaviorX: "contain",
+            // scrollSnapType: "x mandatory",
             transform: `translateX(${-100 * imgIndex}%)`,
           }}
         >
           {slides}
-          <button
-            className="absolute inset-y-0 align-middle"
-            onClick={prevSlide}
-          >
-            <FaChevronLeft />
-          </button>
-          <button
-            className="absolute inset-y-0 align-middle right-0"
-            onClick={nextSlide}
-          >
-            <FaChevronRight />
-          </button>
         </figure>
+        {/* <button className="absolute inset-y-0 align-middle" onClick={prevSlide}>
+          <FaChevronLeft />
+        </button>
+        <button
+          className="absolute inset-y-0 align-middle right-0"
+          onClick={nextSlide}
+        >
+          <FaChevronRight />
+        </button> */}
         <div className="absolute bottom-4 inset-x-0 ">
           <div className="flex items-center justify-center gap-2">
             {slides.map((_, i) => (
               <div
                 key={i}
-                className={`rounded-full transition-all w-3 h-3 bg-white ${
+                className={`rounded-full transition-all w-3 h-3 bg-white drop-shadow-2xl ${
                   imgIndex === i ? "p-2" : "bg-opacity-50"
                 }`}
+                onClick={() => goToSlide(i)}
               />
             ))}
           </div>
         </div>
-        {/* <div className="absolute flex justify-between inset-0">
-          
-        </div> */}
       </section>
     </>
   );
+};
+
+Carousel.propTypes = {
+  children: PropTypes.array.isRequired,
 };
 
 export default Carousel;
